0b0821dfc6e236102f7ea562bf6e5f2a6f096474
abhishek12d <Abhishekd.encircle@gmail.com>
2024-09-20 09:26:54 +0530

    added header

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,7 @@
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['a-us.storyblok.com'],
+    },
+};
 
 export default nextConfig;
--- src/components/common/Navbar/index.jsx
+++ src/components/common/Navbar/index.jsx
@@ -1,20 +1,85 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link';
+import Image from 'next/image';
+
+import { IoIosArrowDown } from "react-icons/io";
 
 import storyblokApi from '@/model/storyBlokApi';
 import WithStoryblok from '@/components/helpers/withStoryblok';
 
-const Navbar = async () => {
-  const data = await storyblokApi("config");
+const Navbar = () => {
+  const [dropdown, setDropdown] = useState(null);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const navbar = await storyblokApi("config");
+      setData(navbar.story.content.header_menu[0]);
+    };
+
+    fetchData();
+  }, []);
+
+  const handleMouseEnter = (index) => {
+    setDropdown(index);
+  };
+
+  const handleMouseLeave = () => {
+    setDropdown(null);
+  };
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <div className='w-full flex sticky top-0 shadow-sm p-7 gap-10'>
-      {data?.story?.content?.header_menu?.map((item) => {
-        return (
-          <Link key={item._uid} href={item.link.url}>{item.name}</Link>
-        )
-      })}
-    </div>
+    <header className='w-full flex items-center sticky top-0 shadow-md px-7 gap-10'>
+      <Link href={data?.logoLink?.url}>
+        <Image src={data?.logo?.filename} alt={data?.alt || "logo"} width={90} height={20} />
+      </Link>
+
+      <div className='flex gap-6'>
+        {data?.navigationList?.map((navigation, index) => (
+          <div
+            className='relative border-b-4 border-transparent hover:border-b-black py-6 transition-all duration-500 cursor-pointer group'
+            key={index}
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseLeave={handleMouseLeave}
+          >
+            <div className='flex gap-1 items-center'>
+              <Link href={navigation?.link?.url} className='text-gray-700 hover:text-gray-900'>
+                {navigation?.name}
+              </Link>
+              <IoIosArrowDown className='group-hover:-rotate-180 duration-300' />
+            </div>
+
+            {navigation?.subItems?.length > 0 && (
+              <div
+                className={`absolute left-0 top-[70px] mt-2 w-40 bg-white shadow-lg rounded-md py-2 ${dropdown === index ? 'block' : 'hidden'}`}
+              >
+                {navigation.subItems.map((subItem, subIndex) => (
+                  <Link
+                    href={subItem?.link?.url}
+                    key={subIndex}
+                    className='block px-4 py-2 text-gray-600 hover:bg-gray-100'>
+                    {subItem?.name}
+                  </Link>
+                ))}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+      <div className='flex ml-auto gap-6'>
+        {data?.actionItems?.map((item, index) => {
+          return (
+            <p key={index}>{item}</p>
+          )
+        })}
+      </div>
+    </header>
   )
 }
 
