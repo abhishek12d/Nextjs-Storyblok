9587c727257161b2ef028ac06f6808bf19b124a3
abhishek12d <Abhishekd.encircle@gmail.com>
2024-10-09 09:33:01 +0530

    added sort dropdown

--- src/views/Collections/Products/index.jsx
+++ src/views/Collections/Products/index.jsx
@@ -1,10 +1,16 @@
 "use client";
 
-import React, { useMemo } from 'react';
-import ProductCard from '@/components/ecommerce/ProductCard';
+import React, { useMemo, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 
+import ProductCard from '@/components/ecommerce/ProductCard';
+
+const sortingOptions =
+    ["Sorting Options", "Price: low to high", "Price: high to low", "Title: A to Z", "Title: Z to A"];
+
 const Products = ({ products }) => {
+    const [sortDropdown, setSortDropdown] = useState(false);
+
     const searchParams = useSearchParams();
 
     const filters = useMemo(() => {
@@ -51,7 +57,33 @@ const Products = ({ products }) => {
 
     return (
         <div className='w-full space-y-8'>
-            <h2 className='subheading'>Casual Wear</h2>
+            <div className='w-full flex items-center justify-between'>
+                <h2 className='subheading'>Casual Wear</h2>
+                <div className='flex gap-10 items-center'>
+                    <h3 className='text-secondary font-normal text-sm'>{filteredProducts?.length} items</h3>
+                    <div class="relative z-10 min-w-[180px]">
+                        <button class="border border-gray-300 text-[13px] font-semibold relative w-full py-2 px-3 bg-white rounded-lg focus:outline-none sm:text-sm cursor-pointer" id="sort" type="button" aria-haspopup="listbox" aria-expanded="false" onClick={() => setSortDropdown(!sortDropdown)}>
+                            <span class="block truncate text-start">Sorting Options</span>
+                            <span class="absolute inset-y-0 right-2 flex items-center">
+                                <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" aria-hidden="true" class="w-5 h-5 text-gray-400" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">4
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M8 9l4-4 4 4m0 6l-4 4-4-4"></path>
+                                </svg>
+                            </span>
+                        </button>
+                        {sortDropdown &&
+                            <ul className='absolute bg-white w-full z-10 p-3 rounded-md'>
+                                {sortingOptions?.map((option) => {
+                                    return (
+                                        <li key={option}>
+                                            <button onClick={() => setSortDropdown(!sortDropdown)} className='text-sm block text-left text-secondary hover:text-primary hover:bg-gray-50 p-2 rounded-md w-full'>{option}</button>
+                                        </li>
+                                    )
+                                })}
+                            </ul>
+                        }
+                    </div>
+                </div>
+            </div>
             <div className='grid grid-cols-1 xs:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 3xl:grid-cols-5 gap-6'>
                 {filteredProducts?.map((product) => (
                     <ProductCard data={product} key={product.id} />
